e1160e3588b40dbeab43621e8dedb9d2e389591d
Hourdakis Emmanouil <ehourdak@ics.forth.gr>
2015-12-22 18:50:55 +0200

    Added a Genetic Algorithm and support for optimization of the liquid.

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
+# __Liquid State Machine__
 
+### _Simulation and optimization of a liquid state machine_
+Implementation details for the liquid are given in: Maass, W. *"Real-time Computing Without Stable States: A New Framework for Neural Computation Based on perturbations."* Neural Computations 14, 2002: 2531-2560.
 
-INSTALLATION:
--------
-Set in cmake the NeMo-0.7.2, boost_1_55_0 and Eigen3 directories.
+## Installation instructions:
+- Use CMake to set the directories of the required libraries. 
+- Generate the project and build it
+- running main will simulate an LSM, and evolve, it to optimize its performance on a given problem.
 
-Then run make.
-To train the LSM run:
-./main
+## Required libraries:
+1. NeMo-0.7.2
+2. boost_1_55_0
+3. Eigen3
+4. Gaul-0.1849
 
-This will train an LSM and stack the results into a best.m matlab file (for fast loading the results into matlab).
+## Tested on:
+* Ubuntu 14.04
 
-
-
-Implementation details for the liquid are given in:
-MAASS, W. "Real-time Computing Without Stable States: A New Framework for Neural Computation Based  on perturbations." Neural Computations 14 (2002): 2531-2560.
+### Computational Vision and Robotics Laboratory (CVRL), Foundation for Research and Technology (FORTH), Hellas
+*contact: Emmanouil Hourdakis (ehourdak@ics.forth.gr)*
--- main.cpp
+++ main.cpp
@@ -3,22 +3,27 @@
 #include "Trainings.h"
 #include "Readouts.h"
 
+extern "C" {
+#undef FUNPROTO
+#include "gaul.h"
+}
+
 #define CHECK_MODE(m) if( viewMode & m )
+#define USE_GA					//Enable this define to include the genetic algorithm
 
 //Application output mode
 enum outMode { outStats 	= 1ul << 0, 
-	       outOutputs	= 1ul << 1, 
-	       outDrawing	= 1ul << 2,
-	       outReadout       = 1ul << 3
+	       outOutputs	= 1ul << 1,
+	       outReadout       = 1ul << 2
 };
-unsigned long 	viewMode 	= outStats | outOutputs;
+unsigned long 	viewMode 	= outStats;
 unsigned long 	lsmMode  	= outStats;
 unsigned int 	nGenerations 	= 40;
 unsigned int 	nChromosomes 	= 40;
 unsigned int 	currentGen   	= 0;
 unsigned int 	simTime 	= 1500;    
 unsigned int 	sampling 	= 5;
-bool 		bSave 		= true;
+bool 		bSave 		= false;
 vector<double> 	all_values;
 TicToc 	    	timer;
 
@@ -88,95 +93,169 @@ void generateMassTraining(Training &trainings, unsigned int simTime, unsigned in
     }
 }
 
+/**
+* @function evolve_lsm
+* @brief Evolves an LSM, and optimizes it to perform a given function
+* 
+* @param population pop The population of chromosomes
+* @param entity entity The GA pointer
+*/ 
+boolean evolve_lsm(population *pop, entity *entity) {      
+    //Generate training data    
+    Training trainings(1, 1);  
+//     generateBinaryTraining(trainings, simTime, sampling);
+//     generateSinTraining(trainings, simTime, sampling);
+    generateMassTraining(trainings, simTime, sampling);
+  
+    if( currentGen != pop->generation ) {
+      currentGen = pop->generation;
+    }
+    
+    printf("\nGeneration %d", pop->generation);
+    double *alleles = ((double *)entity->chromosome[0]);
+    
+    LSM lsm;
+    lsm._outMode = lsmMode;
+    lsm.addPoissonLayer(0, 0, 0, trainings._nInputs, 1, 1, 
+			25.0 + 5.0 * alleles[0]);    	//excitatory percent
+
+    lsm.addLayer(alleles[1], alleles[2], alleles[3], 
+		 10, 	     5, 	 5,
+		80.0 + 2.0 * alleles[4],
+		alleles[4] / 100.0,
+		alleles[5] / 20.0,
+		alleles[6] * (-8.0),
+		alleles[13] / 2.0,
+		alleles[14],
+		alleles[15] * (-3.0), 0.0 ); 
+        
+    lsm.connectPoissonAll	(100.0, 		//percent of connections
+				 0, 0, 
+				alleles[7]/10.0,	//weight mean
+				30.0 );       		//weight variance
+    lsm.connectPoissonAll	(100.0, 		//percent of connections
+				 0, 0, 
+				alleles[7]/10.0,	//weight mean
+				30.0 );       		//weight variance
+    
+    lsm.connectLayer		(0, 0,
+				1000.0f * alleles[8], 	//nConnections	
+				1.0 * alleles[9],	//lambda
+				1.0,//alleles[10],   	//distance metric
+				alleles[11]/10.0,	//weight mean
+				alleles[12]*10.0 );	//weight variance
+    
+    NemoSim sim(&lsm);
+    lsm.init(&sim);          
+    
+    //Start simulating and training the network
+    timer.tic();
+    CHECK_MODE(outStats) cout << endl << "Simulating network" << endl;
+    for( unsigned i = 0; i < trainings._trainings.size(); i++ ) {
+      lsm.priorToSim();
+      for( unsigned n = 0; n < lsm._poissonLayers[0]._nNeurons; n++ ) {
+	  lsm._poissonLayers[0].inputNeuron(&lsm, n, (trainings._trainings[i]._inputs(n)));
+      }        
+      
+      std::vector<unsigned> fired = lsm.simulate();
+      std::sort(fired.begin(), fired.end());      
+    }  
+           
+    timer.toc();                      
+
+    CholeskyReadout readout(&lsm, &trainings, sampling); 
+    MatrixXd solved	= readout.trainData(0);    
+    double fit 		= readout.getError(solved);        
+    
+    VectorXd trainingVector = trainings.generateOutputVector(readout._samplingInterval);
+    
+    CHECK_MODE(outOutputs) {
+      MatrixXd illus(simTime/readout._samplingInterval, 2);
+      illus.col(0) = solved.col(0);
+      illus.col(1) = trainingVector;
+      cout << illus << endl;
+    }
+    
+    if( bSave ) {
+      std::ofstream bestfile;
+      bestfile.open("best.m", std::ofstream::out | std::ofstream::trunc);
+      MatrixXd illus(simTime/readout._samplingInterval, 2);
+      illus.col(0) = solved.col(0);
+      illus.col(1) = trainingVector;
+      bestfile << "a=[ " << illus << endl << "];" << endl;     
+    }
+    
+    printf( RED "\nNormal error: %f\n" RESET, fit );    
+    
+    entity->fitness = 1.0/fit;
+    return TRUE;
+}
+
 int main (int argc, char *argv[])
 {           
-  //Generate training data    
-  Training trainings(1, 1);  
-//   generateBinaryTraining(trainings, simTime, sampling);
-  generateSinTraining(trainings, simTime, sampling);
-//   generateMassTraining(trainings, simTime, sampling);
-          
-  double alleles[16] = {6.131599, 7.394816, 2.184467, 4.553420, 2.660279, 1.068094, 1.204304, 2.400204, 4.776328, 6.516498, 8.549516, 6.460633, 8.381927, 5.145809, 4.437454, 2.312391};
-    
-  LSM lsm;
-  lsm._outMode = lsmMode;
-  lsm.addPoissonLayer(0, 0, 0, trainings._nInputs, 1, 1, 
-		      25.0 + 5.0 * alleles[0]);
-  lsm.addLayer(alleles[1], alleles[2], alleles[3], 
-	      10, 5, 5,
-	      80.0 + 2.0 * alleles[4],
-	      alleles[4] / 100.0,
-	      alleles[5] / 20.0,
-	      alleles[6] * (-8.0),
-	      alleles[13] / 2.0,
-	      alleles[14],
-	      alleles[15] * (-3.0), 0.0 ); 
-      
-  lsm.connectPoissonAll	(100.0, 		
-			 0, 0, 
-			 alleles[7]/10.0,	
-			 30.0 );       		
-  lsm.connectPoissonAll	(100.0, 		
-			 0, 0, 
-			 alleles[7]/10.0,	
-			 30.0 );       		
+  random_seed(20092004);			/* Random seed requires any integer parameter. 	*/
   
-  lsm.connectLayer	(0, 0,
-			 1000.0f * alleles[8], 	
-			 1.0 * alleles[9],	
-			 1.0,		  	
-			 alleles[11]/10.0,	
-			 alleles[12]*10.0 );	
-  
-  NemoSim sim(&lsm);
-  lsm.init(&sim);          
-  
-  //Start simulating and training the network
-  timer.tic();
-  CHECK_MODE(outStats) cout << endl << "Simulating network" << endl;
-  for( unsigned i = 0; i < trainings._trainings.size(); i++ ) {
-    lsm.priorToSim();
-    for( unsigned n = 0; n < lsm._poissonLayers[0]._nNeurons; n++ ) {
-	lsm._poissonLayers[0].inputNeuron(&lsm, n, (trainings._trainings[i]._inputs(n)));
-    }        
-    
-    std::vector<unsigned> fired = lsm.simulate();
-    std::sort(fired.begin(), fired.end());
-    
-#ifdef DEBUG__
-    printf("Simtime: %u Neurons fired: %d     --- ", i, (int)fired.size());
-    for( unsigned i = 0; i < fired.size(); i++ ) {
-      printf("%u ", fired[i]);
-    }
-    printf("\n");
-#endif
-  }
-           
-  timer.toc(true);                      
+  size_t      *  beststrlen;
+  population *pop=NULL;				/* The population of solutions. 		*/
 
-  CholeskyReadout readout(&lsm, &trainings, sampling); 
-  MatrixXd solved	= readout.trainData(0);    
-  double fit 		= readout.getError(solved);        
-  
-  VectorXd trainingVector = trainings.generateOutputVector(readout._samplingInterval);
-  
-  CHECK_MODE(outOutputs) {
-    MatrixXd illus(simTime/readout._samplingInterval, 2);
-    illus.col(0) = solved.col(0);
-    illus.col(1) = trainingVector;
-    cout << illus << endl;
-  }
-    
-  if( bSave ) {
-    std::ofstream bestfile;
-    bestfile.open("best.m", std::ofstream::out | std::ofstream::trunc);
-    MatrixXd illus(simTime/readout._samplingInterval, 2);
-    illus.col(0) = solved.col(0);
-    illus.col(1) = trainingVector;
-    bestfile << "a=[ " << illus << endl << "];" << endl;     
-  }    
+  pop = ga_genesis_double(
+    nChromosomes,                     		/* const int              population_size 	*/
+    1,                      	 		/* const int              num_chromo 		*/
+    16,     					/* const int              len_chromo 		*/
+    NULL,                    			/* GAgeneration_hook      			*/
+    NULL,                     			/* GAiteration_hook       		 	*/
+    NULL,                     			/* GAdata_destructor      		 	*/
+    NULL,                 			/* GAdata_ref_incrementor 		 	*/
+    evolve_lsm,         			/* GAevaluate             evaluate 		*/
+    ga_seed_double_random, 			/* GAseed                 seed 			*/
+    NULL,                     			/* GAadapt                	 		*/
+    ga_select_one_roulette,     		/* GAselect_one           select_one 		*/
+    ga_select_two_roulette,        		/* GAselect_two           select_two 		*/
+    ga_mutate_double_singlepoint_drift, 	/* GAmutate  		  mutate 		*/
+    ga_crossover_double_mixing, 		/* GAcrossover     	  crossover 		*/
+    NULL,                     			/* GAreplace              	 		*/
+    NULL                    			/* void *                 	 		*/ );
 
-  printf( RED "\nReadout error: %f\n" RESET, fit );        
+    ga_population_set_parameters(
+    pop,                     			/* population              *pop 		*/
+    GA_SCHEME_DARWIN,        			/* const ga_class_type     class 		*/
+    GA_ELITISM_PARENTS_DIE,  			/* const ga_elitism_type   elitism 		*/
+    0.9,                     			/* double                  crossover 		*/
+    0.2,                     			/* double                  mutation 		*/
+    0.0               				/* double                  migration 		*/);  
+  
+  ga_population_set_allele_min_double(pop, 1);
+  ga_population_set_allele_max_double(pop, 10);    
+  
+  ga_evolution(
+    pop,                     			/* population              *pop 		*/
+    nGenerations                     		/* const int               max_generations 	*/);
+
+  cout << endl << endl << "====================================";
+  printf( "The final solution found was:\n");
+  printf( "Fitness score = %f Error: %f\n", 
+	  ga_get_entity_from_rank(pop,0)->fitness,
+	  1.0 / ga_get_entity_from_rank(pop,0)->fitness );
+
+  entity *best = ga_get_entity_from_rank(pop,0);
+  double *best_alleles = ((double *)best->chromosome[0]);
+  
+  cout << endl << "Run best solution?" << endl;
+  cin.get();
+  
+  char best_chromosome[256];
+  size_t str_length;
+  ga_chromosome_double_to_string(pop, ga_get_entity_from_rank(pop,0), best_chromosome, &str_length);
+  cout << "Best chromosome is: " << best_chromosome << endl;
+  cin.get();
+
+  bSave 	= true;
+  lsmMode 	= LSM::outNone;
+  viewMode 	= outStats | outOutputs;
+  evolve_lsm(pop, best); 
+  cin.get();
+  
+  ga_extinction(pop);	/* Deallocates all memory associated with the population and it's entities. */       
 
   return 0;
 }
